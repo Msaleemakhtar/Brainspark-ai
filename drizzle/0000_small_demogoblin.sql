a832bd23fefe8d0e14f78bf14c374440aee51e07
Msaleemakhtar <saleemakhtar864@gmail.com>
2023-09-19 17:17:16 +0500

    updated

--- drizzle/0000_small_demogoblin.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS "UserApiLimit" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"userId" varchar,
-	"count" integer DEFAULT 0,
-	"createdAt" timestamp DEFAULT now() NOT NULL,
-	"updatedAt" timestamp DEFAULT now() NOT NULL,
-	CONSTRAINT "UserApiLimit_userId_unique" UNIQUE("userId")
-);
--- lib/drizzle.ts
+++
@@ -1,8 +0,0 @@
-
-
-import { sql } from '@vercel/postgres';
-import { migrate } from "drizzle-orm/postgres-js/migrator";
-import { drizzle } from 'drizzle-orm/vercel-postgres';
- 
-export const db = drizzle(sql)
-await migrate(db, { migrationsFolder: "drizzle" });
